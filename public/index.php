801df92773cc69cf37f36c155ce555115bed663c
Nam Luu <nam.luuduc@gmail.com>
2023-10-28 23:09:01 +0700

    add Route table

--- public/index.php
+++ public/index.php
@@ -13,4 +13,12 @@ require '../Core/Router.php';
 
 $router = new Router();
 
-echo get_class($router);
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+// Display the routing table
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';
