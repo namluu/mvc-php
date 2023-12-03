d07a43ace347333549fc62a51fa3ff6c5658e48d
Nam Luu <nam.luuduc@gmail.com>
2023-12-03 21:34:44 +0700

    optimize db connection

--- App/Models/PostModel.php
+++ App/Models/PostModel.php
@@ -1,24 +1,13 @@
 <?php
 namespace App\Models;
 
-use PDO;
-
-class PostModel
+class PostModel extends \Core\Model
 {
     public static function getAll()
     {
-        $host = 'localhost';
-        $dbname = 'mvc';
-        $username = 'root';
-        $password = '';
-
-        try {
-            $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-            $stmt = $db->query('SELECT id, title, content FROM posts ORDER BY created_at');
-            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            return $results;
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-        }
+        $db = static::getDB();
+        $stmt = $db->query('SELECT id, title, content FROM posts ORDER BY created_at');
+        $results = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        return $results;
     }
 }
---
+++ Core/Model.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Core;
+
+abstract class Model
+{
+    protected static function getDB()
+    {
+        static $db = null;
+
+        if ($db === null) {
+            $host = 'localhost';
+            $dbname = 'mvc';
+            $username = 'root';
+            $password = '';
+
+            try {
+                $db = new \PDO("mysql:host=$host;dbname=$dbname;charset=utf8", 
+                                $username, $password);
+                return $db;
+            } catch (\PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+    }
+}
